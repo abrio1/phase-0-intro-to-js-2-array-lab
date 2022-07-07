49af510cef86c20a671fd8dbcc7b8264d56ee475
Amanda Briones <amandabriones111@gmail.com>
2022-07-06 23:43:12 -0700

    commit tree 34c2f1ff8647e883ced9385a7494343099803f60

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)}
+    function destructivelyPrependCat(name){
+        cats.unshift(name)
+    }
+    function destructivelyRemoveLastCat(){
+        cats.pop()
+    }
+    function destructivelyRemoveFirstCat(){
+        cats.shift()
+    }
+    function appendCat(name){
+        const allCats = [...cats]
+         allCats.push(name);
+         return allCats;
+    }
+    function prependCat(name){
+        const allCats = [...cats]
+        allCats.unshift(name);
+        return allCats;
+    }
+    function removeLastCat(){
+        const allCats = [...cats]
+        allCats.pop()
+        return allCats;
+    }
+    function removeFirstCat(){
+        const allCats = [...cats]
+        allCats.shift()
+        return allCats;
+    }
